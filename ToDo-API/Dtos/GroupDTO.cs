3da3a96f618fbcaeaac9076f4ef32e20c45f24fb
luciobeckler <luciobecklerp@gmail.com>
2025-04-09 11:24:08 -0300

    raw: criando DTOs

---
+++ ToDo-API/Dtos/GroupDTO.cs
@@ -0,0 +1,7 @@
+﻿namespace ToDo_API.Dtos
+{
+    public class GroupDTO
+    {
+        public string Title { get; set; }
+    }
+}
---
+++ ToDo-API/Dtos/ToDoTaskDTO.cs
@@ -0,0 +1,13 @@
+﻿namespace ToDo_API.Dtos
+{
+    public class ToDoTaskDTO
+    {
+        public string Title { get; set; }
+        public string? Description { get; set; }
+        public string Status { get; set; }
+        public string Priority { get; set; }
+        public DateOnly? StartDateTime { get; set; }
+        public DateOnly? EndDateTime { get; set; }
+        public int? GroupId { get; set; }
+    }
+}
